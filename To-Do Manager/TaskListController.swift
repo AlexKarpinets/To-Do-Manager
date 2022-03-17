8854a5fd867e186898408bb004c46cac59925bfd
AlexKarpinets <alexanderkarpinets@gmail.com>
2022-03-17 21:01:32 +0300

    add DidSelectAtRow, lleftbarbutton & sorted method modify,

--- To-Do Manager/TaskListController.swift
+++ To-Do Manager/TaskListController.swift
@@ -10,13 +10,24 @@ import UIKit
 class TaskListController: UITableViewController {
     
     var taskStorage: TaskStorageProtocol = TasksStorage()
-    var tasks: [TaskPriority:[TaskProtocol]] = [:]
     var sectionsTypesPosition: [TaskPriority] = [.important, .normal]
     var taskStatusPosition: [TaskStatus] = [.planned, .completed]
+    var tasks: [TaskPriority:[TaskProtocol]] = [:] {
+        didSet {
+            for (tasksGroupPriority, taskGroup) in tasks {
+                tasks[tasksGroupPriority] = taskGroup.sorted { task1, task2 in
+                    let task1position = taskStatusPosition.firstIndex(of: task1.status) ?? 0
+                    let task2position = taskStatusPosition.firstIndex(of: task2.status) ?? 0
+                    return task1position < task2position
+                }
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTasks()
+        navigationItem.leftBarButtonItem = editButtonItem
     }
     
     // MARK: - Table view data source
@@ -46,6 +57,35 @@ class TaskListController: UITableViewController {
         return title
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let taskType = sectionsTypesPosition[indexPath.section]
+        guard let _ = tasks[taskType]?[indexPath.row] else { return }
+        guard tasks[taskType]![indexPath.row].status == .planned else {
+            tableView.deselectRow(at: indexPath, animated: true)
+            return
+        }
+        tasks[taskType]![indexPath.row].status = .completed
+        tableView.reloadSections(IndexSet(arrayLiteral: indexPath.section), with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let taskType = sectionsTypesPosition[indexPath.section]
+        guard let _ = tasks[taskType]?[indexPath.row] else { return nil }
+        guard tasks[taskType]![indexPath.row].status == .completed else { return nil }
+        
+        let actionSwipeInstance = UIContextualAction(style: .normal, title: "Не выполнена") { _,_,_ in
+            self.tasks[taskType]![indexPath.row].status = .planned
+            self.tableView.reloadSections(IndexSet(arrayLiteral: indexPath.section), with: .automatic)
+        }
+        return UISwipeActionsConfiguration(actions: [actionSwipeInstance])
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let taskType = sectionsTypesPosition[indexPath.section]
+        tasks[taskType]?.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
     private func loadTasks() {
         sectionsTypesPosition.forEach { taskType in
             tasks[taskType] = []
@@ -53,34 +93,27 @@ class TaskListController: UITableViewController {
         taskStorage.loadTasks().forEach { task in
             tasks[task.type]?.append(task)
         }
-        for (tasksGroupPriority, taskGroup) in tasks {
-            tasks[tasksGroupPriority] = taskGroup.sorted { task1, task2 in
-                let task1position = taskStatusPosition.firstIndex(of: task1.status) ?? 0
-                let task2position = taskStatusPosition.firstIndex(of: task2.status) ?? 0
-                return task1position < task2position
-            }
-        }
     }
     
-//    private func getConfiguredTaskCell_constraints(for indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellConstraints", for: indexPath)
-//        let taskType = sectionsTypesPosition[indexPath.section]
-//        guard let currentTask = tasks[taskType]?[indexPath.row] else { return cell }
-//
-//        let symbolLabel = cell.viewWithTag(1) as? UILabel
-//        let textLabel = cell.viewWithTag(2) as? UILabel
-//        symbolLabel?.text = getSymbolForTask(with: currentTask.status)
-//        textLabel?.text = currentTask.title
-//
-//        if currentTask.status == .planned {
-//            textLabel?.textColor = .black
-//            symbolLabel?.textColor = .black
-//        } else {
-//            textLabel?.textColor = .lightGray
-//            symbolLabel?.textColor = .lightGray
-//        }
-//        return cell
-//    }
+    //    private func getConfiguredTaskCell_constraints(for indexPath: IndexPath) -> UITableViewCell {
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellConstraints", for: indexPath)
+    //        let taskType = sectionsTypesPosition[indexPath.section]
+    //        guard let currentTask = tasks[taskType]?[indexPath.row] else { return cell }
+    //
+    //        let symbolLabel = cell.viewWithTag(1) as? UILabel
+    //        let textLabel = cell.viewWithTag(2) as? UILabel
+    //        symbolLabel?.text = getSymbolForTask(with: currentTask.status)
+    //        textLabel?.text = currentTask.title
+    //
+    //        if currentTask.status == .planned {
+    //            textLabel?.textColor = .black
+    //            symbolLabel?.textColor = .black
+    //        } else {
+    //            textLabel?.textColor = .lightGray
+    //            symbolLabel?.textColor = .lightGray
+    //        }
+    //        return cell
+    //    }
     
     private func getConfiguredTaskCell_stack(for indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellStack", for: indexPath) as! TaskCell
